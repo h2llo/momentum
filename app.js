6e6a2e74f7f2fd7f4ee60b0d0aa5512bf5c37a06
h2llo <dy159951@gmail.com>
2023-03-24 13:28:03 +0900

    day 2

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const hellos = document.getElementsByClassName("hello");
+const loginForm = document.querySelector("#login-form");
+const nameInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const userName = nameInput.value;
+  greeting.innerText = "hello " + userName;
+  localStorage.setItem("username", userName);
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
